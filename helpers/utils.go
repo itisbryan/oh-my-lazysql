1c2f7a859eea4c49ef70f72eea1aa5d3272af8ed
itisbryan <nkhoinguyen26@gmail.com>
2026-05-06 11:36:02 +0700

    feat: add BuildConnectionURL helper function

--- helpers/utils.go
+++ helpers/utils.go
@@ -3,7 +3,9 @@ package helpers
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net"
+	"net/url"
 	"strconv"
 	"time"
 
@@ -14,6 +16,40 @@ import (
 
 func ParseConnectionString(url string) (*dburl.URL, error) {
 	return dburl.Parse(url)
+}
+
+func BuildConnectionURL(provider, username, password, hostname, port, dbname string, sslEnabled bool, sslCert, sslKey, sslCA string) (string, error) {
+	switch provider {
+	case "mysql":
+		url := fmt.Sprintf("mysql://%s:%s@%s:%s/%s", url.QueryEscape(username), url.QueryEscape(password), hostname, port, dbname)
+		if sslEnabled {
+			url += "?tls=true"
+			if sslCert != "" || sslKey != "" || sslCA != "" {
+				url += "&tls=true"
+			}
+		}
+		return url, nil
+
+	case "postgres":
+		url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", url.QueryEscape(username), url.QueryEscape(password), hostname, port, dbname)
+		if sslEnabled {
+			url += "?sslmode=require"
+		}
+		return url, nil
+
+	case "sqlite3":
+		if dbname == "" {
+			return "", errors.New("database name required for SQLite")
+		}
+		return fmt.Sprintf("sqlite3:%s", dbname), nil
+
+	case "sqlserver":
+		url := fmt.Sprintf("sqlserver://%s:%s@%s:%s?database=%s", url.QueryEscape(username), url.QueryEscape(password), hostname, port, dbname)
+		return url, nil
+
+	default:
+		return "", errors.New("unsupported provider: " + provider)
+	}
 }
 
 func ContainsCommand(commands []commands.Command, command commands.Command) bool {
